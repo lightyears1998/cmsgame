4b37394da131bfebb58bdbe6d72654d5e790c053
lightyears <lightyears1998@hotmail.com>
2023-06-05 14:53:00 +0800

    将 Input 做成类型

--- Helpers/InputAction.cs
+++ Helpers/InputAction.cs
@@ -1,7 +1,13 @@
 namespace CMSGame
 {
     [InputMap]
-    internal partial class InputAction
+    internal static partial class InputAction
     {
+        public static readonly Dictionary<string, Vector2I> MoveDirections = new(){
+            {InputAction.MoveUp, Vector2I.Up },
+            {InputAction.MoveDown, Vector2I.Down },
+            {InputAction.MoveLeft, Vector2I.Left },
+            {InputAction.MoveRight, Vector2I.Right }
+        };
     }
 }
