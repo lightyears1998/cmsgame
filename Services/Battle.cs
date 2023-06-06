3077c9f148d1b84ba0bdb61c416193f7fb73eab4
lightyears <lightyears1998@hotmail.com>
2023-06-06 17:36:07 +0800

    定义抽象基类

---
+++ Services/Battle.cs
@@ -0,0 +1,15 @@
+namespace CMSGame
+{
+    internal static class Battle
+    {
+        public static BattleResult Fight(IBattleUnit attacker, IBattleUnit defender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BattleResult SimulateFight(IBattleUnit attacker, IBattleUnit defender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
