24aec447b0e89cf80f244664731e74f019e75d56
lightyears <lightyears1998@hotmail.com>
2023-06-07 11:08:01 +0800

    添加注释

--- Helpers/DisplayServerHelper.cs
+++ Helpers/DisplayServerHelper.cs
@@ -1,5 +1,16 @@
 namespace CMSGame
 {
+    /// <summary>
+    /// DisplayServerHelper
+    /// </summary>
+    /// <remarks>
+    /// 关于 Godot 图形显示相关的提示：
+    /// <list type="bullet">
+    /// <item>
+    /// ViewPort 的 Size 包括了标题栏和边框在内整个窗口的大小；而客户区的大小可以通过 DisplayServer.GetWindowSize() 来获得。
+    /// </item>
+    /// </list>
+    /// </remarks>
     internal static class DisplayServerHelper
     {
         public static void ApplyResolutionSettings(bool useFullScreen)
