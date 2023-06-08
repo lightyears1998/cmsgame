d56248ab43125e5d9d2d30733f55b80584fb3798
lightyears <lightyears1998@hotmail.com>
2023-06-08 10:24:23 +0800

    在脚本中连接信号

--- Components/Settings/DeveloperOptionsMenu.cs
+++ Components/Settings/DeveloperOptionsMenu.cs
@@ -3,7 +3,13 @@ namespace CMSGame
     [SceneTree]
     internal partial class DeveloperOptionsMenu : Control
     {
-        public static void On_OpenUserDataDirButton_Pressed()
+        public override void _Ready()
+        {
+            // 连接信号
+            OpenUserDataDirButton.Pressed += OpenUserDataDirButton_Pressed;
+        }
+
+        public void OpenUserDataDirButton_Pressed()
         {
             OS.ShellOpen(new GodotPath("user://"));
         }
